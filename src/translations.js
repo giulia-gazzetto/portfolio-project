d0a12854090ab5f9d8dbaf948367b1a62a912598
gio-gionni <g.gazzetto@gmail.com>
2025-02-21 08:06:14 +0100

    final touches

--- src/translations.js
+++ src/translations.js
@@ -10,6 +10,27 @@ export const translations = {
     introprojectthree:
       "Un sito informativo dedicato al turismo in Ticino, con suggerimenti su luoghi da visitare e attività da fare",
     gotoproject: "Vai ai progetti",
+    aboutme: "Su di me",
+    aboutmetextone:
+      "Sono Giulia, una Junior Front-End Developer con un background in graphic design, appassionata di design, logica e problem-solving. Ciò che mi affascina di più nello sviluppo web è la possibilità di partire da zero e trasformare un’idea in qualcosa di concreto, funzionale e visivamente accattivante. Amo esplorare diversi approcci per trovare il giusto equilibrio tra estetica e usabilità.",
+    aboutmetexttwo:
+      "La mia visione è quella di combinare bellezza e funzionalità creando soluzioni digitali che non siano solo accattivanti, ma anche intuitive e accessibili a tutti.",
+    aboutmetextthree:
+      " Lavoro con tecnologie come React e JavaScript, per sviluppare interfacce fluide e responsive su ogni dispositivo. Sono particolarmente interessata al UI/UX design e sempre alla ricerca di nuovi strumenti e metodologie per migliorare la qualità dei miei progetti.",
+    aboutmetextfour:
+      "    Quando non scrivo codice, mi piace cucire e scrivere, perché credo che la creatività sia alla base di ogni forma di progettazione, anche nello sviluppo web!",
+    skillone: "Problem-solving e conoscenze base di SEO",
+    skilltwo:
+      "Integrazione base di API con Fetch API (creazione di app con data esterni, come app meteo e dizionari).",
+    learning: "Attualmente in fase di apprendimento:",
+    skillthree: "RESTful API e Axios (gestione di richieste e risposte JSON).",
+    cvbtn: "Vai al CV",
+    worksbtn: "I miei lavori",
+    myprojects: "I miei progetti",
+    projectfourtext:
+      "Un’app meteo che fornisce previsioni accurate per qualsiasi città nel mondo, con dati aggiornati in tempo reale.",
+    textme: "Puoi scrivermi qui",
+    mycv: "Il mio CV",
     footer: "Lavoriamo insieme",
   },
   en: {
@@ -22,7 +43,31 @@ export const translations = {
       "An application to search for word definitions, pronunciation, and related information using an external API.",
     introprojectthree:
       "An informational website dedicated to tourism in Ticino, with suggestions on places to visit and activities to do.",
+
     gotoproject: "Go to projects",
+    aboutme: "About me",
+    aboutmetextone:
+      "I am Giulia, a Junior Front-End Developer with a background in graphic design, passionate about design, logic, and problem-solving. What fascinates me most about web development is the ability to start from scratch and turn an idea into something tangible, functional, and visually appealing. I love exploring different approaches to find the right balance between aesthetics and usability.",
+    aboutmetexttwo:
+      "My vision is to combine beauty and functionality by creating digital solutions that are not only visually appealing but also intuitive and accessible to everyone.",
+    aboutmetextthree:
+      "I work with technologies like React and JavaScript to develop smooth and responsive interfaces on any device. I am particularly interested in UI/UX design and always on the lookout for new tools and methodologies to improve the quality of my projects.",
+    aboutmetextfour:
+      "When I’m not writing code, I enjoy sewing and writing because I believe that creativity is at the heart of every form of design, even in web development!",
+    skillone: "Problem-solving and basic SEO knowledge.",
+    skilltwo:
+      "Basic API integration with Fetch API (creating apps with external data, such as weather apps and dictionaries).",
+    learning: "Currently learning: ",
+    skillthree:
+      "RESTful APIs and Axios (handling JSON requests and responses).",
+    cvbtn: "Check my CV",
+    worksbtn: "My works",
+    myprojects: "My projects",
+
+    projectfourtext:
+      "A weather app that provides accurate forecasts for any city around the world, with real-time updated data.",
+    textme: "Say hello",
+    mycv: "My CV",
     footer: "Let's work together",
   },
 };
